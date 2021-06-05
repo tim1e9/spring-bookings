ea1069758c73e68c984727de6ebc7b364675515b
Tim Crowley <tim@crowleyworks.com>
2021-06-05 14:03:10 -0400

    Fixed unit test application properties

--- src/test/java/com/crowleyworks/sample/SampleApplicationTests.java
+++ src/test/java/com/crowleyworks/sample/SampleApplicationTests.java
@@ -4,8 +4,10 @@ import com.crowleyworks.main.BookingApplication;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(classes = BookingApplication.class)
+@TestPropertySource(locations="classpath:application-test.properties")
 class SampleApplicationTests {
 
 	@Test
